c29df3a231619893a1c9e5025b79db27af25aca0
thomasjbhamilton <thomas@reverbconsulting.com>
2016-07-27 14:00:45 -0400

    Add files via upload

---
+++ main.rb
@@ -0,0 +1,16 @@
+class Customer
+
+  attr_reader :name, :budget
+
+  def initialize(name, budget)
+    @name = name
+    @budget = budget
+
+  end
+
+  def within_budget?(price)
+    budget >= price
+  end
+
+
+end
